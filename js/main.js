e7177dedcf30065556d3ba8eb76cba9796203a96
khanjanov <karimkhanjanov@gmail.com>
2024-03-08 18:44:22 +0400

    added sound to popup and fixed js

--- js/main.js
+++ js/main.js
@@ -1,41 +1,56 @@
 $(document).ready(() => {
-  let monthlyBtn = $("#monthly");
-  let yearlyBtn = $("#yearly");
+  $monthlyBtn = $("#monthly");
+  $yearlyBtn = $("#yearly");
+  $chooseSimpleBtn = $("#chooseSimple");
+  $chooseBestBtn = $("#chooseBest");
+  $soundForPopup = $("#soundForPopup");
+  $popup = $(".popup");
+  $heart = $(".fa-heart");
+  $heart.css("cursor", "pointer");
 
-  let baseURL = "http://localhost:3000";
+  $baseURL = "http://localhost:3000";
 
-  $.get(baseURL + "/period", function showData(data) {
-    let monthlyBasic = data[0].monthly[0];
-    let monthlyStandart = data[0].monthly[1];
+  $.get($baseURL + "/period", function showData(data) {
+    $monthlyBasic = data[0].monthly[0];
+    $monthlyStandart = data[0].monthly[1];
+    $yearlyBasic = data[0].yearly[0];
+    $yearlyStandart = data[0].yearly[1];
 
-    getData(monthlyBasic, monthlyStandart);
+    getData($monthlyBasic, $monthlyStandart);
 
-    monthlyBtn.click(() => {
-      if (monthlyBtn.hasClass("active")) {
-        return;
-      } else {
-        toggle();
-        getData(monthlyBasic, monthlyStandart);
-      }
-    });
+    $monthlyBtn.click(
+      { param1: $monthlyBtn, param2: $monthlyBasic, param3: $monthlyStandart },
+      periodTypeEvent
+    );
 
-    yearlyBtn.click(() => {
-      if (yearlyBtn.hasClass("active")) {
-        return;
-      } else {
-        toggle();
-        let yearlyBasic = data[0].yearly[0];
-        let yearlyStandart = data[0].yearly[1];
+    $yearlyBtn.click(
+      { param1: $yearlyBtn, param2: $yearlyBasic, param3: $yearlyStandart },
+      periodTypeEvent
+    );
+  });
 
-        getData(yearlyBasic, yearlyStandart);
-      }
-    });
+  $chooseSimpleBtn.click(() => {
+    chooseEvent($chooseSimpleBtn);
+  });
+
+  $chooseBestBtn.click(() => {
+    chooseEvent($chooseBestBtn);
+  });
+
+  $heart.click(() => {
+    if ($heart.hasClass("fa-regular")) {
+      $heart.removeClass("fa-regular");
+      $heart.toggleClass("fa-solid");
+    } else {
+      $heart.removeClass("fa-solid");
+      $heart.toggleClass("fa-regular");
+    }
   });
 
   // functions for prevent code repetition
   function toggle() {
-    yearlyBtn.toggleClass("active");
-    monthlyBtn.toggleClass("active");
+    $yearlyBtn.toggleClass("active");
+    $monthlyBtn.toggleClass("active");
   }
 
   function getData(basicType, standartType) {
@@ -51,43 +66,32 @@ $(document).ready(() => {
     $("#bestSize").text(standartType.size);
     $("#bestAvailability").text(standartType.availability);
   }
-  let chooseSimpleBtn = $("#chooseSimple");
-  chooseSimpleBtn.click(() => {
-    let simpleOffer = chooseSimpleBtn.parent().children(":first").text();
 
-    let popup = $(".popup");
-    popup.css({
-      left: "4%",
-    });
-    popup.text("you've choosed " + simpleOffer);
-    setTimeout(() => {
-      popup.css("left", "-150%");
-    }, 1300);
-  });
-  let chooseBestBtn = $("#chooseBest");
-  chooseBestBtn.click(() => {
-    let bestOffer = chooseBestBtn.parent().children(":first").text();
+  function periodTypeEvent(e) {
+    $pType = e.data.param1;
+    $pTypeBasic = e.data.param2;
+    $pTypeStandart = e.data.param3;
 
-    let popup = $(".popup");
-    popup.css({
-      left: "4%",
-    });
-    popup.text("you've choosed " + bestOffer);
-    setTimeout(() => {
-      popup.css("left", "-150%");
-    }, 1300);
-  });
-
-  let heart = $(".fa-heart");
-  heart.css("cursor", "pointer");
-
-  heart.click(() => {
-    if (heart.hasClass("fa-regular")) {
-      heart.removeClass("fa-regular");
-      heart.toggleClass("fa-solid");
+    if ($pType.hasClass("active")) {
+      return;
     } else {
-      heart.removeClass("fa-solid");
-      heart.toggleClass("fa-regular");
+      toggle();
+      getData($pTypeBasic, $pTypeStandart);
     }
-  });
+  }
+
+  function chooseEvent(a) {
+    $textOfPlanType = a.parent().children(":first").text();
+
+    $soundForPopup[0].play();
+
+    $popup.css({
+      left: "4%",
+    });
+    $popup.text("you've choosed " + $textOfPlanType);
+
+    setTimeout(() => {
+      $popup.css("left", "-150%");
+    }, 1300);
+  }
 });
